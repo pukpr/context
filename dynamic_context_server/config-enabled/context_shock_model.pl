023c6c8f5aa9a5b91141486ce5c7272f7c2f04f0
pukpr <puk@umn.edu>
2013-02-21 01:08:40 -0600

    shock working

--- dynamic_context_server/config-enabled/context_shock_model.pl
+++ dynamic_context_server/config-enabled/context_shock_model.pl
@@ -1,4 +1,5 @@
 :- module(context_shock_model, [
+				dd/5
 			    ]).
 
 /** <module> Shock model
@@ -12,30 +13,228 @@
 :- context:register(context_shock_model:plot).
 
 
+dd(Exp, C, T, Year, Result) :-
+    Result is C/(1+(T/(Year+0.01))^Exp).
+/*
 shocks(
     [[0, 0.070], % Start of data
-     [42, 0.070], % Start of oil embargo
-     [43, 0.065], % End of oil embargo
-     [48, 0.065], % Start of Iran hostage crisis
-     [49, 0.044], % End of recession
-     [58, 0.044], % Start of Gulf War
-     [60, 0.042], % End of recesssion
-     [69, 0.042], % Last good year!
-     [71, 0.046], % Running Out??? Why is extraction going up?
+     [116, 0.070], % Start of oil embargo
+     [117, 0.065], % End of oil embargo
+     [122, 0.065], % Start of Iran hostage crisis
+     [123, 0.044], % End of recession
+     [132, 0.044], % Start of Gulf War
+     [134, 0.042], % End of recesssion
+     [143, 0.042], % Last good year!
+     [145, 0.046], % Running Out??? Why is extraction going up?
      [300, 0.046]]
+      ).
+*/
+
+shocks(
+    [[0,   0.0], % Start of data
+     [1,   0.025],
+     [108, 0.025], % Vietnam war 1966
+     [116, 0.034], % Start of oil embargo 1974
+     [117, 0.031], % End of oil embargo 1975
+     [121, 0.032], % Start of Iran hostage crisis 1979
+     [124, 0.025], % End of recession 1982
+     [131, 0.024], % Start of Gulf War 1989
+     [134, 0.024], % End of recesssion 1992
+     [144, 0.025], % Last good year! 2002
+     [146, 0.0265], % Running Out??? Why is extraction going up?
+     [300, 0.045]]
       ).
 
 discoveries(
-     [             9.0,  4.0,  4.5,  3.0,  5.0,  5.5, 42.0, 42.5, % 1930's
+     [   0,     0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,    0,  9.0,  4.0,  4.5,  3.0,  5.0,  5.5, 42.0, 42.5, % 1930's
       52.0, 18.0, 16.0,  5.0,  3.0,  7.4,  7.6,  7.0, 49.0, 53.0, % 1940's
       54.0, 19.5, 16.0, 26.0, 20.0, 28.0, 24.0, 35.0, 40.0, 40.5, % 1950's
       42.0, 44.0, 50.0, 41.0, 50.0, 48.5, 47.5, 32.0, 30.5, 30.4, % 1960's
       29.5, 40.5, 37.0, 38.5, 24.0, 26.5, 31.0, 37.0, 38.0, 36.0, % 1970's
       26.0, 24.0, 20.0, 20.0, 22.0, 21.0, 20.5, 18.0, 16.2, 17.5, % 1980's
       16.5, 20.0, 17.0, 16.0,  9.0,  8.5,  9.0,  9.0,  9.5, 14.0, % 1990's
-      18.0, 17.5, 13.0, 9.0,   9.0] % 2004
+      18.0, 17.5, 13.0, 9.0,   9.0,  0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0
+      ]
       ).
+/*
+production(
+     [                 0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,	0,     0,     0, 31803, 34568, 37118, 40436, 43633, 48061,
+     50844, 53666, 58463, 58617, 55824, 60412, 62713, 63331, 66049, 62946,
+     59533, 57296, 56598, 57683, 57468, 60461, 60785, 63160, 64051, 65470,
+     65288, 65788, 66046, 67116, 68103, 69895, 72158, 73586, 72333, 74950,
+     74828, 74443, 77054, 80260,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0]).
+*/
 
+production(
+     [								 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+0.2,
+0.1674,
+0.1818,
+0.1949,
+0.218,
+0.2151,
+0.2133,
+0.2642,
+0.2856,
+0.2987,
+0.3278,
+0.3444,
+0.3524,
+0.3853,
+0.4075,
+0.432,
+0.4575,
+0.5029,
+0.5035,
+0.5559,
+0.6889,
+0.766,
+0.8589,
+1.0157,
+1.0143,
+1.0679,
+1.0968,
+1.263,
+1.325,
+1.486,
+1.41,
+1.373,
+1.31,
+1.442,
+1.522,
+1.654,
+1.792,
+2.039,
+1.988,
+2.086,
+2.15,
+2.221,
+2.093,
+2.257,
+2.593,
+2.595,
+2.745,
+3.022,
+3.433,
+3.404,
+3.803,
+4.283,
+4.519,
+4.798,
+5.018,
+5.626,
+6.125,
+6.439,
+6.608,
+7.134,
+7.6613,
+8.1943,
+8.8878,
+9.5374,
+10.2857,
+11.0704,
+12.6173,
+13.5481,
+14.7591,
+15.926,
+17.5423,
+18.5581,
+19.5881,
+21.3394,
+21.3952,
+20.3761,
+22.05,
+22.8899,
+23.1154,
+24.1075,
+
+21.7385105,	20.45820146,	19.510564,	19.43619562,	19.89211274,	19.69732063,
+20.55884838,	20.67727435,	21.42435208,	21.82766868,	22.08131375,	21.94658422,
+21.93748769,	21.9631248,	22.32831624,	22.7882914,	23.2936527,	24.01917335,
+24.46659495,	24.07809837,	25.01057732,	24.86251254,	24.54996317,	25.31766624,
+26.44878788,	26.88006085,	26.74316637,	26.57759995,	26.87433811,	26.36114676,
+27.01266834,	27.04024902,
+/*
+22.9753,
+21.7292,
+20.913,
+20.6572,
+21.0543,
+20.9762,
+22.0694,
+22.1865,
+23.053,
+23.3779,
+23.8966,
+23.8294,
+24.0115,
+24.1057,
+24.4962,
+24.8572,
+25.5095,
+26.368,
+26.8669,
+26.4161,
+27.3535,
+27.2786,
+27.1494,
+28.1382,
+29.2723,
+29.5971,
+*/
+
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0,
+         0,     0,     0,     0,     0,	    0,     0,     0,	 0,	0]).
 
 %%   navigate(+Request)
 %
@@ -89,10 +288,11 @@ plot(Request) :-
 			      limit(_Limit, [number]),
                               t_units(_TUnits, []),
                               evaluate(_Table, [])]),
-    Time range [0, 150]/1,
+    Time range [0, 250]/1,
     shocks(Shocks),
     interpolate(Time, Shocks, Sh),
-    Data tuple Time + Sh,
+    Date mapdot 1858 .+ Time,
+    Data tuple Date + Sh,
 
     reply_html_page([title('Shock Extraction'),
                      \(con_text:style)],
@@ -107,39 +307,45 @@ plot(Request) :-
 
 plot(Request) :-
     http_parameters(Request, [kind(plot, []),
-			      limit(Limit, [number]),
+			      limit(_Limit, [number]),
                               t_units(_TUnits, []),
                               evaluate(Characteristic, [default(shocked)])]),
 
-    Time range [0, 150]/1,
-    Margin mapdot Limit ~> Time,
-    discoveries(Discoveries),
+    Time range [0, 250]/1,
+    % Margin mapdot Limit ~> Time,
+    production(Profile),
+    Production shrink Profile/Time,
+    discoveries(D),
+    Disc shrink D/Time,
+    DD mapdot dd(7, 2200, 103) ~> Time,
+    Discoveries derivative DD/Time,
     shocks(Shocks),
     (
        Characteristic  = shocked ->
          interpolate(Time, Shocks, Rate)
     ;
        Characteristic = average  ->
-         Rate mapdot 0.05 ~> Time
+         Rate mapdot 0.025 ~> Time
     ),
     Lag mapdot exp(8) ~> Time,
     Fallow convolve Discoveries*Lag,
     Build convolve Fallow*Lag,
     Mature convolve Build*Lag,
     Reserve shrink Mature/Time,
-    Production deplete Reserve/Rate,
+    Model deplete Reserve/Rate,
     % Production shrink Extract/Time,
-    Date mapdot 1932 .+ Time,
-    Data tuple Date + Margin + Production,
+    Date mapdot 1858 .+ Time,
+    Data tuple Date + Production + Model + Discoveries + Disc,
     X = 'Date',
-    Y = 'Production',
+    % Y = 'Model',
     XUnits = ' year',
     YUnits = ' billion barrels/year',
     reply_html_page([title('Shock Profile'),
                      \(con_text:style)],
                     [
-		     \(context_graphing:dygraph_native(lin, [X, 'margin', Y],
-						       [X,XUnits], [Y, YUnits],
+		     \(context_graphing:dygraph_native(lin,
+						       [X, 'Data', 'Model', 'Dispersive', 'Discoveries'],
+						       [X,XUnits], ['Production', YUnits],
 						       Characteristic, Data))
                     ]
 		  ).
