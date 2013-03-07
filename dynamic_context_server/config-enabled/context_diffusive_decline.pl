ec88d4272edb12b15351ced8ae218ea84a496be7
Paul Pukite <puk@umn.edu>
2013-03-07 15:28:33 -0600

    diffusive decline optimization

--- dynamic_context_server/config-enabled/context_diffusive_decline.pl
+++ dynamic_context_server/config-enabled/context_diffusive_decline.pl
@@ -1,6 +1,7 @@
 :- module(context_diffusive_decline, [
 				% dd/5,
-				diffusive_cumulative/4
+				diffusive_cumulative/4,
+                                reciprocal_power/3
 			    ]).
 
 /** <module> Diffusive Decline model
@@ -9,6 +10,7 @@
 */
 
 :- use_module(context_math).
+:- use_module(library('R')).
 
 :- context:register(context_diffusive_decline:navigate).
 :- context:register(context_diffusive_decline:plot).
@@ -16,6 +18,9 @@
 
 diffusive_cumulative(Median, Max, X, Y) :-
     Y is Max/(1+sqrt(Median/(X+0.0000001))).
+
+reciprocal_power(Power, X, Y) :-
+    Y is 1.0/X^Power.
 
 %%   navigate(+Request)
 %
@@ -61,6 +66,23 @@ navigate(Request) :-
 		  ).
 
 
+get_line(X, Y, Slope, Int, R2) :-
+   r_open_session,
+   x <- X,
+   y <- Y,
+   fitxy <- lm('y~x'),
+   r_print(fitxy),
+   Slope <- 'as.double(fitxy$coefficients[2])',
+   Int <- 'as.double(fitxy$coefficients[1])',
+   % r_in(summary(fitxy)),
+   summary <- summary(fitxy),
+   R2 <- 'as.double(summary$r.squared)',
+   r_close.
+
+calculate_fit([_|X], [_|Y], Slope, Int, R2) :-
+   XR mapdot reciprocal_power(0.5) ~> X,
+   YR mapdot reciprocal_power(1.0) ~> Y,
+   get_line(XR, YR, Slope, Int, R2).
 
 %%   plot(+Request)
 %
@@ -87,42 +109,51 @@ plot(Request) :-
 		  ).
 
 
-
 plot(Request) :-
     http_parameters(Request, [kind(plot, []),
-			      median(Median, [number]),
+			      median(_Median, [number]),
                               t_units(_TUnits, []),
                               evaluate(Characteristic, [default(cumulative)])]),
 
     Time range [0, 46]/1,
     production(Profile),
-    sumlist(Profile, Max),
-    Prod mapdot diffusive_cumulative(Median, Max) ~> Time,
+    Prof mapdot 365 .* Profile,
+    % sumlist(Prof, Max),
+
+    Cumulative accumulate Prof,
+    calculate_fit(Time, Cumulative, Slope, Intercept, R2),
+    EUR is 1.0/Intercept,
+    Tau is (EUR*Slope)^2,
+    Prod mapdot diffusive_cumulative(Tau, EUR) ~> Time,
     (
        Characteristic  = cumulative ->
-         Cumulative accumulate Profile,
+         YUnits = ' cumulative barrels',
 	 Data tuple Time + Cumulative + Prod,
          Heading = ['Time', 'Data', 'Model']
     ;
        Characteristic = ou  ->
+         YUnits = ' cumulative barrels',
 	 Data tuple Time + Prod,
          Heading = ['Time', 'Model']
     ;
        Characteristic = yearly  ->
+         YUnits = ' fractional decline',
 	 Data tuple Time + Prod,
          Heading = ['Time', 'Model']
     ;
        Characteristic = ou_yearly  ->
+         YUnits = ' fractional decline',
 	 Data tuple Time + Prod,
          Heading = ['Time', 'Model']
     ),
     X = 'Date',
     XUnits = ' year',
-    YUnits = ' fractional decline',
     reply_html_page([title('Shock Profile'),
                      \(con_text:style)],
                     [
-                     b('Max='),i(Max),
+                     b('EUR='),i(EUR),
+                     b('  Median='), i(Tau),
+                     b('  R2='), i(R2),
 		     \(context_graphing:dygraph_native(lin,
 						       Heading,
 						       [X,XUnits], ['Production', YUnits],
