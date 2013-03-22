00229ede9a8b950565d3499122cdd028bbc7a575
Paul Pukite <puk@umn.edu>
2013-03-22 17:25:34 -0500

    MCV addition

--- dynamic_context_server/config-enabled/context_stats.pl
+++ dynamic_context_server/config-enabled/context_stats.pl
@@ -18,7 +18,8 @@ msv(List, Var) :-
     Z ordinal X,
     Y mapdot 1.0 .+ Z,
     Vars mapdot Y * X,
-    Var mapdot Scale .* Vars.
+    Vars2 mapdot Y * Vars,
+    Var mapdot Scale .* Vars2.
 
 msv(_, 0, X, X) :- !.
 msv(List, N, X, Y) :-
@@ -29,7 +30,7 @@ msv(List, N, X, Y) :-
               Diff is (S1-T1)*(S1-T1)),
              Group),
     sumlist(Group, Sum),
-    M is N - 1,
+    M is N - 1, !,
     msv(List, M, [Sum|X], Y).
 
 
