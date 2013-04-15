5456b8c061df21fc0eae35fa5331c346025f417a
Paul Pukite <puk@umn.edu>
2013-04-15 15:56:38 -0500

    more stats functions

--- dynamic_context_server/config-enabled/context_stats.pl
+++ dynamic_context_server/config-enabled/context_stats.pl
@@ -1,5 +1,12 @@
 :- module(context_stats, [
-		       msv/2
+		       msv/2,
+                       expsm/3,
+                       absdev/3,
+                       dist/3,
+                       mean/2,
+                       moment/3,
+                       median/2,
+                       rms/2
                      ]).
 
 /** <module>  Statistics operations on arrays
@@ -34,3 +41,90 @@ msv(List, N, X, Y) :-
     msv(List, M, [Sum|X], Y).
 
 
+%%   expsm(+X, +A, -Y)
+%
+%    Exponential smoother
+%
+expsm([], _, _, Y_In, Y_Out) :- reverse(Y_In, Y_Out).
+expsm([X|X_In], A, Sum, Y_In, Y_Out) :-
+    Y is (1-A)*X + A*Sum,
+    expsm(X_In, A, Y, [Y|Y_In], Y_Out).
+
+expsm(X, A, Y) :-
+    expsm(X, A, 0.0, [], Y).
+
+
+
+%%   absdev(+X, +A, -Y)
+%
+%    Absolute deviation from scalar A
+%
+absdev([],_,Y, Y).
+absdev([X|X_In],A,Y_In, Y_Out) :-
+    Y is abs(X-A) + Y_In,
+    absdev(X_In,A,Y, Y_Out).
+
+absdev(X, A, Y) :-
+    absdev(X,A,0,Y).
+
+%%   dist(+X, +A, -Y)
+%
+%    Norm squared deviation from scalar A
+%
+dist([],_,Y, Y).
+dist([X|X_In],A,Y_In, Y_Out) :-
+    Y is (X-A)*(X-A) + Y_In,
+    dist(X_In,A,Y, Y_Out).
+
+dist(X, A, Y) :-
+    dist(X,A,0,Y).
+
+%%   mean(+X, -Y)
+%
+%    Mean of array
+%
+mean([],Y, Y).
+mean([X|X_In],Y_In, Y_Out) :-
+    Y is X + Y_In,
+    mean(X_In,Y, Y_Out).
+
+mean(X, Y) :-
+    length(X, N),
+    mean(X, 0.0, Y0),
+    Y is Y0/N.
+
+
+%%   moment(+X, +A, -Y)
+%
+%    Moment A of array
+%
+moment([],_,Y, Y).
+moment([X|X_In],A,Y_In, Y_Out) :-
+    Y is X**A + Y_In,
+    moment(X_In, A, Y, Y_Out).
+
+moment(X, A, Y) :-
+    length(X, N),
+    moment(X, A, 0.0, Y0),
+    Y is Y0/N.
+
+%%   median(+X, -Y)
+%
+%    Median of array
+%
+median(X, Y) :-
+    msort(X, XS),
+    length(X, N),
+    NF is N/2+1,
+    N2 is floor(NF),
+    nth1(N2, XS, Y).
+
+%%   rms(+X, -Y)
+%
+%    RMS of array
+%
+rms(X, Y) :-
+    mean(X, M),
+    length(X, N),
+    dist(X, M, Y0),
+    Y is sqrt(Y0/N).
