d80c0b47d145c339652fd036d2f2c81896a8a4bd
pukpr <puk@umn.edu>
2015-06-14 15:10:25 -0500

    tiles

--- dynamic_context_server/config-enabled/track_grid2.pl
+++ dynamic_context_server/config-enabled/track_grid2.pl
@@ -2,7 +2,10 @@
 :- context:register(track_grid2:navigate).
 :- context:register(track_grid2:plot).
 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  These are the available types of tiles
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 tile(blank).
 tile(cross).
 tile(curveNE).
@@ -28,13 +31,16 @@ substitute_group([blank]).
 substitute_group([cross]).
 
 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % This is the database of tiles = a(Front,Back)
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 a(curveSE, lineE).
 a(teeS, lineE).
-a(curveSW, curveNE). % NE
+a(lineS, curveNE).
 a(curveSW, parkN).
 a(blank, lineS).
-a(curveSE, curveNW).
+a(lineE, curveNW).
 a(curveSE, teeS).
 a(teeW, curveNE).
 a(curveSW, teeE).
@@ -46,10 +52,10 @@ a(blank, cross).
 
 a(curveSE, lineE).
 a(teeS, lineE).
-a(curveSW, curveNE).
+a(curveSW, blank).
 a(curveSW, parkN).
 a(blank, lineS).
-a(curveSE, curveNW).
+a(lineE, curveNW).
 a(curveSE, teeS).
 a(teeW, curveNE).
 a(curveSW, teeE).
@@ -58,8 +64,6 @@ a(parkE, teeN).
 a(curveNW, parkN).
 a(blank, blank).
 
-
-/*
 a(curveSE, lineE).
 a(teeS, lineE).
 a(teeW, curveNE).
@@ -68,7 +72,7 @@ a(curveSE, teeS).
 a(cross, curveNW).
 a(curveSW, teeE).
 a(parkE, teeN).
-a(curveSW, curveNE).
+a(blank, curveNE).
 a(curveSW, parkN).
 a(blank, lineS).
 a(curveSE, curveNW).
@@ -76,10 +80,10 @@ a(blank, cross).
 
 a(curveSE, lineE).
 a(teeS, lineE).
-a(curveSW, curveNE).
+a(cross, curveNE).
 a(curveSW, parkN).
 a(blank, lineS).
-a(curveSE, curveNW).
+a(blank, curveNW).
 a(curveSE, teeS).
 a(teeW, curveNE).
 a(curveSW, teeE).
@@ -90,7 +94,7 @@ a(blank, blank).
 
 a(curveSE, lineE).
 a(teeS, lineE).
-a(curveSW, curveNE).
+a(blank, curveNE).
 a(curveSW, parkN).
 a(blank, lineS).
 a(curveSE, curveNW).
@@ -100,18 +104,16 @@ a(curveSW, teeE).
 a(cross, curveNW).
 a(parkE, teeN).
 a(curveNW, parkN).
-a(blank, blank).
 
-*/
+a(blank, lineS).
+a(blank, lineE).
+a(lineS, blank).
+a(lineS, blank).
 
 
-sub_group(Piece, Sub) :-
-   substitute_group(G),
-   member(Piece, G),
-   member(Sub, G).
-
-either_side(Front, [Sub,_]) :- sub_group(Front, Sub).
-either_side(Back,  [_,Sub]) :- sub_group(Back,  Sub).
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% These are track configurations
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 config(0,
        [curveSE, curveSW,
@@ -120,6 +122,10 @@ config(0,
 config(100,
        [cross, cross,
 	cross, cross]).
+config(1000,
+       [curveSE, lineE, curveSW,
+	lineS, blank, lineS,
+        curveNE, lineE, curveNW ]).
 
 config(1,
        [curveSE, lineE, teeS, lineE, curveSW,
@@ -135,20 +141,66 @@ config(2,
 	curveNE, teeN, cross, lineE, teeW,
 	blank, parkE, curveNW, blank, parkN]).
 
+config(3,
+       [curveSE, lineE, teeS, lineE, curveSW,
+	lineS, blank, lineS, blank, lineS,
+	teeE, lineE , cross, lineE, teeW,
+	lineS, blank, lineS, blank, lineS,
+	curveNE, lineE, teeN, lineE, curveNW]).
 
-available([], _, 'all parts available!') :- !.
-available([N|Needed], From, Result) :-
+config(4,
+       [parkS, curveSE, teeS, lineE, parkW,
+	teeE, curveNW, curveNE, teeS, curveSW,
+	lineS, blank , blank, teeE, teeW,
+	teeE, curveSW, curveSE, teeN, curveNW,
+	parkN, curveNE, teeN, lineE, parkW]).
+
+config(5,
+       [curveSE, lineE, teeS, lineE, curveSW,
+	teeE, parkW, lineS, parkS, lineS,
+	lineS, blank, lineS, lineS, lineS,
+	teeE, parkW, curveNE, cross, teeW,
+	curveNE, lineE, lineE, teeN, curveNW]).
+
+config(6,
+       [curveSE, curveSW, parkS, curveSE, curveSW,
+	curveNE, cross, teeN, cross, curveNW,
+	blank, lineS, blank, lineS, blank,
+	curveSE, cross, teeS, cross, curveSW,
+	curveNE, curveNW, parkN, curveNE, curveNW]).
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% These are the rules
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+sub_group(Piece, Sub) :-
+   substitute_group(G),
+   member(Piece, G),
+   member(Sub, G).
+
+either_side(Front, [Sub,_]) :- sub_group(Front, Sub).
+either_side(Back,  [_,Sub]) :- sub_group(Back,  Sub).
+
+
+% available([], _, 'All parts available!') :- !.
+available([], _, R,RR) :- reverse(R,RR),!.
+available([N|Needed], From, List, Result) :-
    % member(Piece, From),
    either_side(N, Piece),
    member(Piece, From),
-   selectchk(Piece, From, Remainder),
-   available(Needed, Remainder, Result).
+   select(Piece, From, Remainder),
+   available(Needed, Remainder, [Piece|List], Result).
 % available([N|_], _, Text) :-
 %   concat(N, ' extra parts needed!', Text), !.
 
 find_available(Plan, From,  Result) :-
    findall([Front,Back], a(Front,Back), From ), !,
-   available(Plan, From,  Result), !.
+   available(Plan, From, [], Result).
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%  User Interface
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 make_gif(Part, GIF) :-
    atomic_list_concat(['/html/images/', Part, '.gif'], GIF).
@@ -191,16 +243,17 @@ process_each(N, L, Part) -->
    {
     make_gif(Part, GIF),
     M is N - 1,
-    XPos is 100*(M mod L),
+    XPos is 100*(M mod L) + 250,
     YPos is 100*(M div L),
-    format(atom(Style),'position:absolute; top:~wpx; left:~wpx; width:100px; height:100px; border:none;', [YPos,XPos])
+    format(atom(Style),
+	   'position:absolute; top:~wpx; left:~wpx; width:100px; height:100px; border:none;',
+	   [YPos,XPos])
    },
    html(
        [img([src=GIF,
 	     style=Style])
        ]).
 
-% process(0,'none') --> html([p(none)]), !.
 process(_,_,[]) --> !.
 process(N,L,[First|Rest]) -->
    {M is N +1},
@@ -217,29 +270,50 @@ process(N,L,[First|Rest]) -->
 show_each_part([]) --> !.
 show_each_part([[F,B]|Rest]) -->
    {
+        length(Rest,N),
 	make_gif(F, FGIF),
 	make_gif(B, BGIF)
    },
    html(
-       [img([src=FGIF, width=50]), b(' flip-over '),
+       [i(N), img([src=FGIF, width=50]), b(' flip-over '),
 	img([src=BGIF, width=50]),
 	p([])
        ]),
    show_each_part(Rest).
 
+show_each_part([], _, _) --> !.
+show_each_part([[F,B]|Rest], From, Members) -->
+   {
+        % length(Rest,N),
+	make_gif(F, FGIF),
+	make_gif(B, BGIF),
+        nth0(N,From,[F,B]),
+        not(member(N,Members))
+   },
+   html(
+       [i(N), img([src=FGIF, width=50]), b(' flip-over '),
+	img([src=BGIF, width=50]),
+	p([])
+       ]),
+   show_each_part(Rest,From, [N|Members]).
 
 plot(Request) :-
     http_parameters(Request, [x(X, [number])]),
     config(X,Plan),
     length(Plan, Length),
     Len is integer(sqrt(Length)),
-    find_available(Plan,_List, Result),!,
+    find_available(Plan,List, Result),!,
+    reverse(List,RList),
     reply_html_page([title('Track Layout'),
                      \(con_text:style)],
                     [
-		     h1([Result]),
-		     \process(0, Len, Plan)
-		     % \show_each_part(Plan)
+		     h1('Parts used from those available (scroll down)'),
+		     \process(0, Len, Plan),
+		     \show_each_part(Result,RList,[])
                     ]
 		  ).
+
+
+
+
 
